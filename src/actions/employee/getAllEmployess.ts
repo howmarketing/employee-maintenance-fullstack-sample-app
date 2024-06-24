d12d1dd1942825f708530882e10e7aa3d6fa1653
myblenet <myblenet@gmail.com>
2024-06-24 20:01:49 -0300

    feat(actions): getAllEmployees /api/get-all-employees

--- src/actions/employee/getAllEmployess.ts
+++ src/actions/employee/getAllEmployess.ts
@@ -5,7 +5,7 @@ import { Department, Employee } from "@prisma/client";
 export interface IGetAllEmployessResponse extends Employee {
     department: Department
 }
-export async function getAllEmployessAction(): Promise<Array<IGetAllEmployessResponse>> {
+export async function getAllEmployeesAction(): Promise<Array<IGetAllEmployessResponse>> {
 	// http://localhost:3000/api/get-all-employees
 	const url = `${process.env?.BASE_URL || "http://localhost:3000"}/api/get-all-employees`;
 	const employees = await fetch(url, {
