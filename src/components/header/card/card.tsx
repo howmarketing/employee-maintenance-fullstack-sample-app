8ca0f8be0c9d268c51524bb512e0a6d20a5afd40
myblenet <myblenet@gmail.com>
2024-06-25 09:31:27 -0300

    feat(ui-employee-card): Creating the card base structure component

---
+++ src/components/header/card/card.tsx
@@ -0,0 +1,7 @@
+export const Card = () => {
+	return (
+		<div className="rounded-xl col-span-1 col-start-1 bg-purple-900 justify-center items-center align-middle flex flex-wrap flex-col">
+			<h1 className="text-white">Card</h1>
+		</div>
+	)
+}
