d7de5adf298716a7bc1f1d2c64893dbd8ae19734
howmarketing <gabsariza@icloud.com>
2024-08-06 11:48:16 -0300

    moving dev.db to /tmp directory for vercel access

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ npm i --force
 npm i --legacy-peer-deps
 npm i -D jest @types/jest @testing-library/jest-dom ts-jest @testing-library/user-event jest-environment-jsdom @testing-library/react --force
 npx prisma generate
+cp ./prisma/dev.db /tmp/dev.db
 # Build your project
 npm run build
 
