3b00241c9d63f3cfdd1e91b755b2484093bf4bd0
howmarketing <gabsariza@icloud.com>
2024-08-06 09:52:54 -0300

    Removing .env file

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install dependencies
+npm i --force
+npm i --legacy-peer-deps
+npm i -D jest @types/jest @testing-library/jest-dom ts-jest @testing-library/user-event jest-environment-jsdom @testing-library/react --force
+
+# Build your project
+npm run build
+
+# Run tests
+npm run test
